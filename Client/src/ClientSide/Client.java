266b4e5f8c18970bdcf47da155d12fe5cb940269
Mike <aerothosis@gmail.com>
2015-05-13 20:50:02 -0700

    Done

--- Client/src/ClientSide/Client.java
+++ Client/src/ClientSide/Client.java
@@ -56,6 +56,89 @@ public class Client extends JFrame{
 	//connect to server
 	private void connectToServer() throws IOException{
 		showMessage("Attempting connection... \n");
+		connection = new Socket(InetAddress.getByName(serverIP), 6789);
+		showMessage("Connected to: " + connection.getInetAddress().getHostName());
+	}
+	
+	//setup streams to send and receive messages
+	private void setupStreams() throws IOException{
+		output = new ObjectOutputStream(connection.getOutputStream());
+		output.flush();
+		input = new ObjectInputStream(connection.getInputStream());
+		showMessage("\n Dude your streams are now good to go! \n");
+	}
+	
+	//while chatting with server
+	private void whileChatting() throws IOException{
+		ableToType(true);
+		do{
+			try{
+				message = (String) input.readObject();
+				showMessage("\n" + message);
+			}catch(ClassNotFoundException cnfe){
+				showMessage("\n I don't know that object type... :/");
+			}
+		}while(!message.equals("SERVER - END"));
+	}
+	
+	//close the streams and sockets
+	private void closeCrap(){
+		showMessage("\n Closing crap down...");
+		ableToType(false);
+		try{
+			output.close();
+			input.close();
+			connection.close();
+		}catch(IOException ioe){
+			ioe.printStackTrace();
+		}
+	}
+	
+	//send messages to server
+	private void sendMessage(String message){
+		try{
+			output.writeObject("CLIENT - " + message);
+			output.flush();
+			showMessage("\nCLIENT - " + message);
+		}catch(IOException ioe){
+			chatWindow.append("\n Something messed up sending the message dude.. Sorry..");
+		}
+	}
+	
+	//change OR update chatWindow
+	private void showMessage(final String m){
+		SwingUtilities.invokeLater(
+			new Runnable(){
+				public void run(){
+					chatWindow.append(m);
+				}
+			}
+		);
+	}
+	
+	//gives user permission to type crap into the text box
+	private void ableToType(final boolean tof){
+		SwingUtilities.invokeLater(
+			new Runnable(){
+				public void run(){
+					userText.setEditable(tof);
+				}
+			}
+		);
 	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
